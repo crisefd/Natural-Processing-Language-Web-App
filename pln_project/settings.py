ae458351e5fc434a9a8e919cec157afa5988d0a0
crisefd <cristhian.fuertes@correounivalle.edu.co>
2016-05-15 14:38:58 +0000

    base.html bug fixed

--- pln_project/settings.py
+++ pln_project/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).ancestor(2)
-
+print BASE_DIR
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
