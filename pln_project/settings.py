76a73275b5cc45da08cc4f098d8c7f50857f89a0
crisefd <cristhian.fuertes@correounivalle.edu.co>
2016-05-15 05:07:57 +0000

    route setting bug

--- pln_project/settings.py
+++ pln_project/settings.py
@@ -13,7 +13,7 @@ from unipath import Path
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).ancestor(3)
+BASE_DIR = Path(__file__).ancestor(2)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
--- pln_project/urls.py
+++ pln_project/urls.py
@@ -20,5 +20,5 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('r^', include('apps.freeling_app.urls', namespace='freeling')),
+    url(r'^', include('apps.freeling_app.urls', namespace='freeling')),
 ]
