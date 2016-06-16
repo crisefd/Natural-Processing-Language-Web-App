9f94ae2d22b120babba1176d1035e316622da943
crisefd <cristhian.fuertes@correounivalle.edu.co>
2016-06-16 19:48:52 +0000

    dbparser added

--- apps/syntactic_app/views.py
+++ apps/syntactic_app/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from django.conf import settings
 from helpers.stanford_variables import *
 from django.views.decorators.csrf import csrf_exempt
+import os
 
 def pos_tag(txt):
     return stanford_postagger.tag(txt.split())
@@ -15,10 +16,13 @@ def to_bikel_format(input_array):
         bikel_txt += "(" + item[0] + " " + item_1 + ")"
     return bikel_txt + ")"
 
-def print_to_file(bikel_txt):
-    f = open(settings.BASE_DIR + '/syn_ana_files/bikel-pos.txt', 'w')
-    f.write(bikel_txt)
+def print_to_file(txt, name=''):
+    f = open(settings.BASE_DIR + '/syn_ana_files/' + name, 'w')
+    f.write(txt)
     f.close()
+
+def dan_bikel_parser():
+    os.system("{0}/static/dbparser/bin/parse 1000 {0}/static/dbparser/settings/collins.properties {0}/static/wsj/wsj-02-21.obj.gz {0}/syn_ana_files/bikel-pos.txt".format(settings.BASE_DIR))
 
 @csrf_exempt
 def analysis_view(request):
@@ -28,13 +32,16 @@ def analysis_view(request):
         print "the request method is POST"
         try:
             text = request.POST['text'].decode('utf-8')
-            print 'the text is ' + text
+            #print 'the text is ' + text
             tagged_text = pos_tag(text)
+            print "text tagged"
             bikel_txt = to_bikel_format(tagged_text)
-            print_to_file(bikel_txt)
+            print "bikel formatted text"
+            print_to_file(bikel_txt, name='bikel-pos.txt')
+            dan_bikel_parser()
             print "adding data"
         except Exception as err:
-            print("Error: "+ str(err))
+            print "Error: "+ str(err)
             output['data'] = 'Error ' + str(err)
     print "==> response sent to client"
     return JsonResponse(output)
